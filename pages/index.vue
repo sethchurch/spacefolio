d0cec1107f9e2c5046d75145692e0bec2a587c9d
gschurch <gschurch@my.waketech.edu>
2022-02-08 16:09:50 -0500

    Fixed landing page z stacking issue

--- pages/index.vue
+++ pages/index.vue
@@ -9,7 +9,7 @@
         <header class="landing__header">
           <h1 class="landing__header-text">Hey, <br>I'm Seth<span class="text--orange">.</span></h1>
           <h2 class="landing__subheader text--blue">Software Engineer</h2>
-          <a class="landing__link" href="/contact"><button class="landing__btn btn--orange btn--rounded">Contact Me!</button></a>
+          <a class="landing__link" href="/contact"><button class="landing__btn btn--orange btn--outline btn--rounded">Contact Me!</button></a>
         </header>
       </div>
       <canvas class="landing__canvas" id="landingCanvas" />
