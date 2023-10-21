5d26a86d1f67f40d5329883b6eba509ddf42e3f1
sethchurch <sethcdev@gmail.com>
2023-10-21 15:57:31 -0400

    update git links

--- pages/index.vue
+++ pages/index.vue
@@ -35,7 +35,7 @@
           title="Course Tracker Mobile" 
           desc="A native android application developed in C# using the Xamarin Forms framework and designed to aid students to track their courses for a term and keep an eye on any upcoming due dates. It utilizes an SQLite database to store and organize user-relevant data and an interface that was written in XAML." 
           imgSrc="https://camo.githubusercontent.com/02da002b5866c049b8a4e62fcc7bfff13c4635b14dc90d42cfca67ebb35f0964/68747470733a2f2f692e696d6775722e636f6d2f32677659574f332e706e67"  
-          gitLink="https://github.com/iEnder/Course-Tracker-Mobile" 
+          gitLink="https://github.com/sethchurch/Course-Tracker-Mobile" 
         />
 
         <PortfolioPlanet 
@@ -44,7 +44,7 @@
           title="Torchbearer Armory Bot" 
           desc="A Discord.JS application designed to maintain and organize inventory for a Dungeon and Dragons community server. Programmed entirely in vanilla javascript and hosted on Netlify with a MongoDB database hosted on mLab." 
           imgSrc="https://camo.githubusercontent.com/074efab13f1e347846539e047ac1cd10644b19bc611ed15149e7aec0df876312/68747470733a2f2f692e696d6775722e636f6d2f4b4b6f657268622e706e67"  
-          gitLink="https://github.com/iEnder/Discord5eArmory" 
+          gitLink="https://github.com/sethchurch/discord-armory-5e" 
         />
 
         <PortfolioPlanet 
@@ -53,7 +53,7 @@
           title="Community Leaderboard" 
           desc="A Leaderboard web app which was developed to track and display scores from a community game server. Designed in Illustrator and implemented in Vue.JS, it interfaces a GraphQL API to pull and display the latest information about the players." 
           imgSrc="https://i.imgur.com/vbsylK9.png" 
-          gitLink="https://github.com/iEnder/bb-leaderboards"
+          gitLink="https://github.com/sethchurch/bb-leaderboards"
         />
       </div>
     </section>
--- pages/work/index.vue
+++ pages/work/index.vue
@@ -20,7 +20,7 @@
           title="Course Tracker Mobile"
           desc="A native android application developed in C# using the Xamarin Forms framework and designed to aid students to track their courses for a term and keep an eye on any upcoming due dates. It utilizes an SQLite database to store and organize user-relevant data and an interface that was written in XAML."
           imgSrc="https://camo.githubusercontent.com/02da002b5866c049b8a4e62fcc7bfff13c4635b14dc90d42cfca67ebb35f0964/68747470733a2f2f692e696d6775722e636f6d2f32677659574f332e706e67"
-          gitLink="https://github.com/iEnder/Course-Tracker-Mobile"
+          gitLink="https://github.com/sethchurch/Course-Tracker-Mobile"
         />
 
         <PortfolioDisplay
@@ -28,7 +28,7 @@
           title="Torchbearer Armory Bot"
           desc="A Discord.JS application designed to maintain and organize inventory for a Dungeon and Dragons community server. Programmed entirely in vanilla javascript and hosted on Netlify with a MongoDB database hosted on mLab."
           imgSrc="https://camo.githubusercontent.com/074efab13f1e347846539e047ac1cd10644b19bc611ed15149e7aec0df876312/68747470733a2f2f692e696d6775722e636f6d2f4b4b6f657268622e706e67"
-          gitLink="https://github.com/iEnder/discord-armory-5e"
+          gitLink="https://github.com/sethchurch/discord-armory-5e"
         />
 
         <PortfolioDisplay
@@ -36,7 +36,7 @@
           title="Community Leaderboard"
           desc="A Leaderboard web app which was developed to track and display scores from a community game server. Designed in Illustrator and implemented in Vue.JS, it interfaces a GraphQL API to pull and display the latest information about the players."
           imgSrc="https://i.imgur.com/vbsylK9.png"
-          gitLink="https://github.com/iEnder/bb-leaderboards"
+          gitLink="https://github.com/sethchurch/bb-leaderboards"
         />
 
         <PortfolioDisplay
@@ -44,7 +44,7 @@
           title="Client Scheduler"
           desc="A C# scheduler application designed to manage multiple customers and their associated appointments."
           imgSrc="https://camo.githubusercontent.com/c3754adcd690eabe6efbc8864a80f8b1a0eaf99b8d242409e1b857e23ee1a1f6/68747470733a2f2f692e696d6775722e636f6d2f4d764f506230372e706e67"
-          gitLink="https://github.com/iEnder/Client-Scheduler"
+          gitLink="https://github.com/sethchurch/Client-Scheduler"
         />
 
         <PortfolioDisplay
@@ -52,7 +52,7 @@
           title="Shouter"
           desc="Social media platform inspired by twitter built on NodeJS with the Express framework."
           imgSrc="https://i.imgur.com/Ei1617F.png"
-          gitLink="https://github.com/iEnder/Shouter"
+          gitLink="https://github.com/sethchurch/Shouter"
         />
       </section>
     </div>
